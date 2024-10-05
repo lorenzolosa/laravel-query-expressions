ca2787ada334f41af30d000bea0db46a098d6193
Lorenzo Losa <lorenzo.losa@otxgroup.com>
2024-10-05 17:20:50 +0000

    introduce LastDayOfMonth

---
+++ src/Function/Time/LastDayOfMonth.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tpetry\QueryExpressions\Function\Time;
+
+use Illuminate\Contracts\Database\Query\Expression;
+use Illuminate\Database\Grammar;
+use Tpetry\QueryExpressions\Concerns\IdentifiesDriver;
+use Tpetry\QueryExpressions\Concerns\StringizeExpression;
+
+class LastDayOfMonth implements Expression
+{
+    use IdentifiesDriver;
+    use StringizeExpression;
+
+    public function __construct(
+        private readonly string|Expression $expression,
+    ) {
+    }
+
+    public function getValue(Grammar $grammar)
+    {
+        $expression = $this->stringize($grammar, $this->expression);
+
+        return match ($this->identify($grammar)) {
+            'mariadb', 'mysql' => "LAST_DAY({$expression})",
+            'pgsql' => "DATE_TRUNC('month', {$expression}) + interval '1 month - 1 day'",
+            'sqlite' => "DATE({$expression}, 'start of month', '+1 month', '-1 day')",
+            'sqlsrv' => "EOMONTH({$expression})",
+        };
+    }
+}
---
+++ tests/Function/Time/LastDayOfMonthTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Tpetry\QueryExpressions\Function\Time\LastDayOfMonth;
+use Tpetry\QueryExpressions\Value\Value;
+
+
+it('can compute the last day of month of a column')
+    ->expect(new LastDayOfMonth('date'))
+    ->toBeExecutable()
+    ->toBeMysql('LAST_DAY(`date`)')
+    ->toBePgsql('DATE_TRUNC(\'month\', "date") + interval \'1 month - 1 day\'')
+    ->toBeSqlite('DATE("date", \'start of month\', \'+1 month\', \'-1 day\')')
+    ->toBeSqlsrv('EOMONTH([date])');
+
+it('can compute the last day of month of a date')
+    ->expect(new LastDayOfMonth(new Value('2024-01-03')))
+    ->toBeExecutable()
+    ->toBeMysql("LAST_DAY('2024-01-03')")
+    ->toBePgsql("DATE_TRUNC('month', '2024-01-03') + interval '1 month - 1 day'")
+    ->toBeSqlite("DATE('2024-01-03', 'start of month', '+1 month', '-1 day')")
+    ->toBeSqlsrv('EOMONTH(\'2024-01-03\')');
